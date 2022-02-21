54a8ad1cbb12ade56c6246e691e474b20a7ba847
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-02-21 12:30:35 +1100

    Working on character movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [Tooltip("Movement Values")]
+    [SerializeField] float speedMultiplier, rotatationSpeed, gravityForce, jumpForce, groundCastRange;
+
+    //Components
+    CharacterController cc;
+    Animator anim;
+
+    Vector3 movementDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        if(h != 0 || v != 0)
+        {
+            movementDirection.Set(h, 0, v);
+            cc.Move(movementDirection * speedMultiplier * Time.deltaTime);
+            anim.SetBool("HasInput", true);
+        }
+        else
+        {
+            anim.SetBool("HasInput", false);
+        }
+
+        var desiredDirection = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredDirection, rotatationSpeed);
+
+        var animationVector = transform.InverseTransformDirection(cc.velocity);
+
+        anim.SetFloat("ForwardMomentum", animationVector.z);
+        anim.SetFloat("SideMomentum", animationVector.x);
     }
 }
