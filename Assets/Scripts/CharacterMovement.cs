4808514a1f7aa13f5f1363645399b6f504063f66
amurray56 <87687643+amurray56@users.noreply.github.com>
2022-02-24 18:58:15 +1100

    working on character movement and control
    also worked on animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -10,21 +10,69 @@ public class CharacterMovement : MonoBehaviour
     //Components
     CharacterController cc;
     Animator anim;
-    public GameObject focalPoint;
-    private Transform focalPointTransform;
 
     Vector3 movementDirection;
+    Vector3 playerVelocity;
+    bool groundedPlayer;
+    public Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
-        focalPointTransform = focalPoint.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        groundedPlayer = cc.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            if (anim.GetBool("Jumping")) anim.SetBool("Jumping", false);
+            playerVelocity.y = 0f;
+        }
+       
+        ProcessGravity();
+        Move();
+    }
+
+    public void ProcessGravity()
+    {
+        if(Input.GetButtonDown("Jump") && !anim.GetBool("Jumping"))
+        {
+            anim.SetBool("Jumping", true);
+            playerVelocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravityForce);
+        }
+        playerVelocity.y += gravityForce * Time.deltaTime;
+        cc.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+        
+        if(verticalMove != 0 || horizontalMove != 0)
+        {
+            movementDirection.Set(horizontalMove, 0, verticalMove);
+            anim.SetBool("HasInput", true);
+            cc.Move(speedMultiplier * Time.deltaTime * move);
+        }
+        else
+        {
+            anim.SetBool("HasInput", false);
+        }
+
+        var animationVector = transform.InverseTransformDirection(cc.velocity);
+
+        anim.SetFloat("ForwardMomentum", animationVector.z);
+        anim.SetFloat("SideMomentum", animationVector.x);
+    }
+
+    void DanielRMoveScript()
     {
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
@@ -39,8 +87,6 @@ public class CharacterMovement : MonoBehaviour
         {
             anim.SetBool("HasInput", false);
         }
-
-        transform.LookAt(focalPointTransform);
 
         var desiredDirection = Quaternion.LookRotation(movementDirection);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredDirection, rotatationSpeed);
